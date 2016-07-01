0982156b13d4b89efb60d17b9c4dd24831984696
zengjf <zengjf42@163.com>
2016-07-01 23:02:09 +0800

    add source code to repository.

---
+++ SQLiteMACW/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace SQLiteMACW
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.addMAC = new System.Windows.Forms.Button();
+            this.macB = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.macE = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // addMAC
+            // 
+            this.addMAC.Location = new System.Drawing.Point(200, 21);
+            this.addMAC.Name = "addMAC";
+            this.addMAC.Size = new System.Drawing.Size(75, 50);
+            this.addMAC.TabIndex = 0;
+            this.addMAC.Text = "Add MAC";
+            this.addMAC.UseVisualStyleBackColor = true;
+            this.addMAC.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // macB
+            // 
+            this.macB.Location = new System.Drawing.Point(75, 21);
+            this.macB.Name = "macB";
+            this.macB.Size = new System.Drawing.Size(119, 21);
+            this.macB.TabIndex = 1;
+            this.macB.Text = "00:00:00:00:00:00";
+            this.macB.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "MAC Begin:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "MAC End:";
+            // 
+            // macE
+            // 
+            this.macE.Location = new System.Drawing.Point(75, 50);
+            this.macE.Name = "macE";
+            this.macE.Size = new System.Drawing.Size(119, 21);
+            this.macE.TabIndex = 4;
+            this.macE.Text = "00:00:00:00:00:00";
+            this.macE.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(289, 96);
+            this.Controls.Add(this.macE);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.macB);
+            this.Controls.Add(this.addMAC);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button addMAC;
+        private System.Windows.Forms.TextBox macB;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox macE;
+    }
+}
+
---
+++ SQLiteMACW/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SQLiteMACW
+{
+    public partial class Form1 : Form
+    {
+
+        byte[] macBArray = new byte[6];
+        byte[] macEArray = new byte[6];
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            translateMac();
+            swapMac();
+        }
+
+        void translateMac()
+        {
+            String macBString = macB.Text;
+            String macEString = macE.Text;
+
+            String[] macBStrings = macBString.Split(':');
+            String[] macEStrings = macEString.Split(':');
+
+            for (int i = 0; i < 6; i++)
+                macBArray[i] = byte.Parse(macBStrings[i], System.Globalization.NumberStyles.HexNumber);
+            Console.WriteLine("{0}, {1}, {2}, {3}, {4}, {5}", macBArray[0], macBArray[1], macBArray[2], macBArray[3], macBArray[4], macBArray[5]);
+
+            for (int i = 0; i < 6; i++)
+                macEArray[i] = byte.Parse(macEStrings[i], System.Globalization.NumberStyles.HexNumber);
+            Console.WriteLine("{0}, {1}, {2}, {3}, {4}, {5}", macEArray[0], macEArray[1], macEArray[2], macEArray[3], macEArray[4], macEArray[5]);
+        }
+
+        void swapMac(){
+            int i = 0;
+            int j = 0;
+            byte[] tempArray = new byte[6];
+            for(; i < 6; i++ )
+            {
+                if (macBArray[i] > macEArray[i])
+                {
+                    for (; j < 6; j++)
+                    {
+                        tempArray[j] = macBArray[j];
+                        macBArray[j] = macEArray[j];
+                        macEArray[j] = tempArray[j];
+                    }
+                    break;
+                }
+            }
+        }
+
+        void copyByteArray(byte[] oldArray, byte[] newArray) {
+            
+        }
+
+        void calculateNextMacAddress(int[] mac, int i)
+        {
+
+            int macByte5 = ( i + mac[5] ) % 256;
+            int macByte4 = ( ( i + mac[5] ) / 256  + mac[4] ) % 256;
+            int macByte3 = ( ( ( ( i + mac[5] ) / 256 ) + mac[4] ) / 256  + mac[3] ) % 256;
+            int macByte2 = ( ( ( ( ( i + mac[5] ) / 256 ) + mac[4] ) / 256  + mac[3] ) / 256  + mac[2] ) % 256;
+            int macByte1 = ( ( ( ( ( ( i + mac[5] ) / 256 ) + mac[4] ) / 256  + mac[3] ) / 256  + mac[2] ) / 256 + mac[1] ) % 256;
+            int macByte0 = ( ( ( ( ( ( ( i + mac[5] ) / 256 ) + mac[4] ) / 256  + mac[3] ) / 256  + mac[2] ) / 256 + mac[1] ) / 256 + mac[0] ) % 256;
+
+            mac[0] = macByte0;
+            mac[1] = macByte1;
+            mac[2] = macByte2;
+            mac[3] = macByte3;
+            mac[4] = macByte4;
+            mac[5] = macByte5;
+
+            Console.WriteLine( "calculate next Mac : %02x:%02x:%02x:%02x:%02x:%02x", macByte0, macByte1, macByte2, macByte3, macByte4, macByte5 );
+        }
+
+    }
+}
